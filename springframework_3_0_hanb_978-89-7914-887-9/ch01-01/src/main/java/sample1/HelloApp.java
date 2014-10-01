746bbb8f26f487c05373d1190fa030179efff5fe
Alex Yang <movingmt@gmail.com>
2014-10-01 14:11:34 +0900

    create HelloApp class of smaple1.

---
+++ springframework_3_0_hanb_978-89-7914-887-9/ch01-01/src/main/java/sample1/HelloApp.java
@@ -0,0 +1,8 @@
+package sample1;
+
+public class HelloApp {
+	public static void main(String[] args) {
+		MessageBean bean = new MessageBean();
+		System.out.println(bean.sayHello("Spring"));
+	}
+}
